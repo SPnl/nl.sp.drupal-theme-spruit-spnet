b71fc5eb3652348f9738ec579ab14360d2dfa61a
Stijn Dekker <sdekker@sp.nl>
2019-02-05 11:39:55 +0100

    Cookie compliance bar support

---
+++ templates/eu-cookie-compliance-popup-info.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * This is a template file for a pop-up prompting user to give their consent for
+ * the website to set cookies.
+ */
+?>
+<div class ="popup-content info cookie-consent">
+    <div id="popup-text">
+      <p>De SP gebruikt cookies om het bezoek aan onze sites te meten. Deze gebruiken geen persoonsgegevens. Alleen als je toestemming geeft gebruiken we daarnaast cookies en scripts om advertenties af te stemmen op jouw interesses. Dan verwerken we wel persoonlijke gegevens over je bezoek. Meer informatie vind je op onze <a href="/privacy">privacy pagina</a>.</p>
+    </div>
+    <div id="popup-buttons">
+      <button type="button" class="agree-button">Akkoord</button>
+      <button type="button" class="decline-button eu-cookie-compliance-default-button hollow" >Niet akkoord</button>
+    </div>
+</div>
